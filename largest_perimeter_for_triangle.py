7004740f1b1521974cc2115c542666f12f3c1607
sanjanakm <sanjanahoskote21@gmail.com>
2026-04-14 22:19:09 +0530

    solved new problem

---
+++ largest_perimeter_for_triangle.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def largestPerimeter(self, nums):
+        nums.sort()
+
+        for i in range(len(nums)-1,1,-1):
+            if nums[i-2]+nums[i-1]>nums[i]:
+                return nums[i]+nums[i-1]+nums[i-2]
+            
+        return 0
+
+                
